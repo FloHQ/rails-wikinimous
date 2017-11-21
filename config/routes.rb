3651939aec3a44d5c4350fbb5b47f8b06093b21d
Florian Herlicq <fherlicq@gmail.com>
2017-11-21 17:42:49 +0100

    A working version (trekkie style) of the wikinimous app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'article/index'
+
+  # get 'article/show'
+
+  # get 'article/new'
+
+  # get 'article/create'
+
+  # get 'article/edit'
+
+  # get 'article/update'
+
+  # get 'article/destroy'
+
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
